00a08e4151fbd9713cb5d3ba4471d5cfe223c6ac
sopheaknova <novadesign.dev@gmail.com>
2014-04-09 17:17:44 +0700

    implement search tour custom post type

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php get_header(); ?>
 	<?php get_search_form(); ?>
 	<section id="home">
-		<h1>Welcome to Travella</h1>
+		<h1>Welcome to homepage</h1>
 		<p>This is a <a href="demo.html">simple</a> welcome page.</p>
 	</section>
-	<!-- #content -->
+
+	<?php get_template_part('library/content/searchform-tour'); ?>
         
 <?php get_footer(); ?>
     
--- templates/template-search.php
+++ templates/template-search.php
@@ -4,12 +4,10 @@ Template Name: Search page
 */
 get_header(); ?>
 	<div id="main" role="main">
-        
-        <?php
-        global $wp_query;
-        $total_results = $wp_query->found_posts;
-        echo $total_results;
-        ?>
+    <h3>Custom search page</h3>    
+        <?php if (isset($_GET['term'])) {
+		echo $_GET['term'];
+	} ?>
     	
     </div><!-- #main -->
 <?php get_sidebar(); ?>
