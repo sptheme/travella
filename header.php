dc9fc7447af5143e34b5e6cbfdf283b9a0f27bc0
sopheak.peas <sopheak.peas@novacambodia.com>
2014-06-25 23:52:46 +0700

    Styling heading

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
+	<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" /> -->
 
 	<title><?php wp_title('|', true, 'right'); ?></title>
 
